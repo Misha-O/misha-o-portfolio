3c808279cbaa225421c391c1816d440257416192
Misha Oleinychenko <m.oleinychenko@gmail.com>
2021-03-13 15:07:10 -0500

    fixed layout, added hover effects

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
     const isProductionBuild = argv.mode === "production";
-    const publicPath = "/misha-o-portfolio/";
+    const publicPath = "/oleinychenko-ls/";
 
     const pcss = {
         test: /\.(p|post|)css$/,
